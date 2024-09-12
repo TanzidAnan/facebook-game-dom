b2d42555098ad2c6d76a31356ffe2be4cf6cc651
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-12 23:40:48 +0600

    funcation js

--- scrip.js
+++ scrip.js
@@ -1,28 +1,29 @@
-const allPost =document.querySelector('.allPost');
-    allPost.innerHTML="ht"
-let arr=[
+const allPost = document.querySelector('.allPost');
+let arr = [
     {
-        name:'tanzid',
+        name: 'tanzid',
         caption: 'Hello'
     },
     {
-        name:'marzan',
+        name: 'marzan',
         caption: 'Hi'
     },
     {
-        name:'Anan',
+        name: 'Anan',
         caption: 'Hi anan'
     },
 ];
 
-arr.map(item =>{
-    console.log()
-    allPost.innerHTML= `<div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">${item.name}</h5>
-                    <p class="card-text">${item.caption}</p>
-                    <a href="#" class="btn btn-primary">Edit</a>
-                    <a href="#" class="btn btn-danger">Delete</a>
-                </div>
-            </div>`
-})
+function display() {
+    arr.map(item => {
+        console.log()
+        allPost.innerHTML += `<div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${item.name}</h5>
+                        <p class="card-text">${item.caption}</p>
+                        <a href="#" class="btn btn-primary">Edit</a>
+                        <a href="#" class="btn btn-danger">Delete</a>
+                    </div>
+                </div>`
+    })
+}
